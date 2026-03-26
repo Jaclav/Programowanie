599382ec0334f8c2feb8d01505691f89fe8fef45
Jacek Kazimierz Winiarczyk <jw479686@op016.odk.fuw.edu.pl>
2026-03-26 10:30:01 +0100

    Cw5.2

--- cw5/textcalc.py
+++ cw5/textcalc.py
@@ -2,16 +2,16 @@
 
 import sys
 
-i = input()
-print(eval(i))
-
-i = i.split(" ")
-C = {
-    "+": lambda a, b: a + b,
-    "-": lambda a, b: a - b,
-    "*": lambda a, b: a * b,
-    "/": lambda a, b: a / b,
-    "%": lambda a, b: a % b,
-    "^": lambda a, b: a**b,
-}
-print(C[i[1]](float(i[0]), float(i[2])))
+with open(sys.argv[1], "r") as f:
+    with open(sys.argv[2], "w") as w:
+        for l in f.readlines():
+            i = l.split(" ")
+            C = {
+                "+": lambda a, b: a + b,
+                "-": lambda a, b: a - b,
+                "*": lambda a, b: a * b,
+                "/": lambda a, b: a / b,
+                "%": lambda a, b: a % b,
+                "^": lambda a, b: a**b,
+            }
+            w.write("%s = %.2f\n" % (l.strip(), C[i[1]](float(i[0]), float(i[2]))))
